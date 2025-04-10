f7acf2d8dc4cab1c6b68b8fd1aa16a82026a17df
Giuliano Giovanini <gil@uparte.com.br>
2025-04-10 12:53:53 -0300

    implement named parameters

--- src/Messages/Components/Parameters/Text.php
+++ src/Messages/Components/Parameters/Text.php
@@ -2,17 +2,19 @@
 
 namespace MissaelAnda\Whatsapp\Messages\Components\Parameters;
 
+use Illuminate\Support\Str;
 use MissaelAnda\Whatsapp\Messages\Message;
 
 class Text implements Message
 {
-    public static function create(string $text = ''): static
+    public static function create(string $text = '', ?string $name = null): static
     {
-        return new static($text);
+        return new static($text, $name);
     }
 
     public function __construct(
         public string $text,
+        public ?string $name = null,
     ) {
         //
     }
@@ -23,12 +25,22 @@ class Text implements Message
         return $this;
     }
 
+    public function name(string $name): static
+    {
+        $this->name = Str::snake($name);
+        return $this;
+    }
+
     public function toArray()
     {
-        return [
+        $payload = [
             'type' => 'text',
             'text' => str_replace(["\n", "\t", "\r"], ['\n', '\t', '\r'], $this->text),
         ];
+        if ($this->name) {
+            $payload['paramter_name'] = $this->name;
+        }
+        return $payload;
     }
 
     public function jsonSerialize(): mixed
